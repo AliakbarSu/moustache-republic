649b87af156e9286aeb2c2c63fa69925791481e8
Ali Sultani <aliakbar.su@hotmail.com>
2023-07-31 11:08:04 +1200

    Fetch product data

--- src/App.tsx
+++ src/App.tsx
@@ -2,33 +2,30 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Product from './components/Product'
+import { Product as ProductType } from './types/product'
+import { useEffect, useState } from 'react'
+import axios from 'axios'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [product, setProduct] = useState<ProductType | null>(null)
+
+  useEffect(() => {
+    if (product) return
+    axios
+      .get(import.meta.env.VITE_API as string)
+      .then((res) => {
+        setProduct(res.data)
+      })
+      .catch((err) => {
+        console.log(err)
+      })
+  })
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div id="app">
+        {product ? <Product product={product} /> : <p>Loading...</p>}
+    </div>
   )
 }
 
